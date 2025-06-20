4af45f19f9e75dca9601074fcd2747d763d806af
Igor Limeira <igor.limeira@outlook.com>
2025-06-19 23:04:32 -0300

    feat: first project commit

---
+++ purchase-orders/src/test/java/br/com/geosapiens/purchaseorders/PurchaseOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geosapiens.purchaseorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
