9498581d7dc8abae14b900839b7c679cf152cdbb
Igor Limeira <igor.limeira@outlook.com>
2025-06-21 23:29:12 -0300

    feat(order): created POST method for orders submits

--- purchase-orders/src/main/java/br/com/geosapiens/purchaseorders/controllers/OrderController.java
+++ purchase-orders/src/main/java/br/com/geosapiens/purchaseorders/controllers/OrderController.java
@@ -1,6 +1,13 @@
 package br.com.geosapiens.purchaseorders.controllers;
 
+import br.com.geosapiens.purchaseorders.dtos.ResponseOrderDTO;
+import br.com.geosapiens.purchaseorders.dtos.SubmitOrderDTO;
+import br.com.geosapiens.purchaseorders.interfaces.OrderServiceInterface;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class OrderController {
 
-    //Criação de pedidos: Endpoint REST para criação de pedidos/
+    private OrderServiceInterface orderService;
 
-    // Consulta de status do pedido: Endpoint REST para verificar a situação do pedido (Em andamento, Entregue, Cancelado.
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseOrderDTO> submitOrder(@RequestBody SubmitOrderDTO submitOrderDTO) {
+        return ResponseEntity.ok(orderService.submitOrder(submitOrderDTO));
+    }
+
+    //TODO: Consulta de status do pedido: Endpoint REST para verificar a situação do pedido (Em andamento, Entregue, Cancelado.
 }
