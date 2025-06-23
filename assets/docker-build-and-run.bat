0495555b41ec3ce9b1c6c4c18372bbd65fe126f0
Igor Limeira <igor.limeira@outlook.com>
2025-06-22 21:04:01 -0300

    build: included docker network create.

--- assets/docker-build-and-run.bat
+++ assets/docker-build-and-run.bat
@@ -4,6 +4,19 @@ REM Build Docker images for the Auth and Purchase Order APIs
 SET SCRIPT_DIR=%~dp0
 cd /d %SCRIPT_DIR%\..
 
+REM Check if the network exists
+FOR /F "tokens=*" %%i IN ('docker network ls --filter name=^network1$ --format "{{.Name}}"') DO (
+    IF /I "%%i"=="network1" (
+        echo Docker network "network1" already exists.
+        GOTO BuildImages
+    )
+)
+
+REM Create the Docker network
+echo Creating Docker network "network1"...
+docker network create network1
+
+:BuildImages
 REM Build Auth API image
 echo Building eucomida-auth Docker image...
 docker build -t eucomida-auth -f auth\Dockerfile .
